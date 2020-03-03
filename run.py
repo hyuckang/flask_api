9b7f2f442dba5a5bf6ca98bee3a1af927b797dd0
hyuckang <hyuckang15@gmail.com>
2020-03-04 00:06:39 +0900

    fix typo

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import flask, render_template
+from flask import Flask, render_template
 import json
 app = Flask(__name__)
 
@@ -15,10 +15,15 @@ def hello_server():
     'http://[ip주소]:[port번호]/hello' 를 요청하면
     응답으로 json을 돌려준다.
 '''
-@app.route("hello")
+@app.route("/hello")
 def hello():
     return jsonify({'response' : 'world'})
     
 
+'''
+    host = 0.0.0.0를 하게되면 IP로 접근 가능하다.
+    port 번호는 1234
+    debug = True를 하게 되면 소스코드가 저장 될 때마다 flask가 자동으로 재시작된다.
+'''
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=1234)
+    app.run(host="0.0.0.0", port=1234, debug=True)
